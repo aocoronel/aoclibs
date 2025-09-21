46fa43f53923a6ba57ab43fb57436811fc3fe7d0
Augusto Coronel <aoc@getgoogleoff.me>
2025-09-21 14:56:05 -0300

    feat: add zsh autocompletion generator

---
+++ automation/zshgen.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+#include "zshgen.h"
+
+const char *find_comparg(const char *arg_name, struct ProgramArguments *args,
+                         int args_count) {
+        for (int i = 0; i < args_count; i++) {
+                if (args[i].name != NULL &&
+                    strcmp(args[i].name, arg_name) == 0) {
+                        return args[i].completions;
+                }
+        }
+        return NULL;
+}
+
+void generate_zsh_completion(const CompletionInfo *info) {
+        const ProgramInfo *pinfo = info->info;
+        // Header
+        printf("#compdef %s\n\n", pinfo->name);
+        // Environment defaults
+        for (int i = 0; i < info->envc; i++) {
+                printf("%s=%s\n", info->envs[i].name, info->envs[i].value);
+        }
+        // Main function
+        printf("_%s() {\n", pinfo->name);
+        printf("  local -a subcommands\n\n");
+        // Define Subcommands
+        printf("  subcommands=(\n");
+        for (int i = 0; i < pinfo->cmdc; i++) {
+                printf("    \"%s:%s\"\n", pinfo->commands[i].cmd,
+                       pinfo->commands[i].cmd_desc);
+        }
+        printf("  )\n\n");
+        // Define arguments
+        printf("  _arguments -C \\\n");
+        printf("    '1:command:->subcmds' \\\n");
+        for (int i = 0; i < pinfo->flagc; i++) {
+                const char *short_flag = pinfo->flags[i].short_flag;
+                const char *long_flag = pinfo->flags[i].long_flag;
+                const char *arg = pinfo->flags[i].argument;
+                const char *desc = pinfo->flags[i].desc;
+                const char *comp = (arg != NULL) ? find_comparg(arg, info->args,
+                                                                info->argc) :
+                                                   NULL;
+                if (long_flag) {
+                        printf("    '%s", long_flag);
+                        if (desc) printf("=[%s]", desc);
+                        if (arg) {
+                                if (comp) {
+                                        printf(":%s:_%s_get_%s", arg,
+                                               pinfo->name, arg);
+                                } else {
+                                        printf(":%s", arg);
+                                }
+                        }
+                        printf("' \\\n");
+                }
+                if (short_flag) {
+                        printf("    '%s", short_flag);
+                        if (desc) printf("[%s]", desc);
+                        if (arg) {
+                                if (comp) {
+                                        printf(":%s:_%s_get_%s", arg,
+                                               pinfo->name, arg);
+                                } else {
+                                        printf(":%s", arg);
+                                }
+                        }
+                        printf("' \\\n");
+                }
+        }
+        printf("    '*::args:->command_args'\n\n");
+
+        // Autocompletion
+        printf("  case $state in\n");
+        printf("    subcmds)\n");
+        printf("      _describe 'command' subcommands\n");
+        printf("      return\n");
+        printf("      ;;\n");
+        printf("    command_args)\n");
+        printf("      case $words[1] in\n");
+
+        // Autocomplete arguments from flags and commands
+        for (int i = 0; i < pinfo->cmdc; i++) {
+                const char *cmd = pinfo->commands[i].cmd;
+                const char *cmdargs = pinfo->commands[i].argument;
+                if (cmdargs == NULL) continue;
+                const char *completions =
+                        find_comparg(cmdargs, info->args, info->argc);
+                if (completions == NULL) continue;
+
+                printf("        %s)\n", cmd);
+                printf("          _arguments \\\n");
+                printf("            '*:%s:_%s_get_%s' \\\n", cmdargs,
+                       info->info->name, cmdargs);
+                printf("          return\n");
+                printf("          ;;\n");
+        }
+        for (int i = 0; i < pinfo->flagc; i++) {
+                const char *short_flag = pinfo->flags[i].short_flag;
+                const char *long_flag = pinfo->flags[i].long_flag;
+                const char *flagargs = pinfo->flags[i].argument;
+                if (flagargs == NULL) continue;
+                const char *completions =
+                        find_comparg(flagargs, info->args, info->argc);
+
+                if (completions == NULL) continue;
+
+                if (short_flag != NULL && long_flag != NULL) {
+                        printf("        %s | %s)\n", short_flag, long_flag);
+                } else if (short_flag != NULL) {
+                        printf("        %s)\n", short_flag);
+                } else if (long_flag != NULL) {
+                        printf("        %s)\n", long_flag);
+                }
+                printf("          _arguments \\\n");
+                printf("            '*:%s:_%s_get_%s' \\\n", flagargs,
+                       info->info->name, flagargs);
+                printf("          return\n");
+                printf("          ;;\n");
+        }
+        printf("      esac\n");
+        printf("      ;;\n");
+        printf("  esac\n");
+        printf("}\n\n");
+
+        // Define helper functions to autocomplete arguments
+        for (int i = 0; i < info->argc; i++) {
+                const char *name = info->args[i].name;
+                const char *completions = info->args[i].completions;
+                if (completions != NULL) {
+                        printf("_%s_get_%s() {\n", pinfo->name, name);
+                        printf("  local results\n");
+                        printf("  results=(${(f)\"$(%s 2>/dev/null)\"})\n",
+                               completions);
+                        printf("  compadd -Q -a results\n");
+                        printf("}\n\n");
+                }
+        }
+
+        // Assign function to program
+        printf("compdef _%s %s\n", pinfo->name, pinfo->name);
+}
---
+++ automation/zshgen.h
@@ -0,0 +1,86 @@
+#ifndef BASHGEN_H
+#define BASHGEN_H
+
+#ifndef PROGRAM_VAR
+#define PROGRAM_VAR
+/*
+ * Define the program command details
+ *
+ * command, argument, description
+ */
+struct ProgramCommands {
+  const char *cmd;
+  const char *argument;
+  const char *cmd_desc;
+};
+
+/*
+ * Define the program flag details
+ *
+ * short flag, long flag, argument, description
+ */
+struct ProgramFlag {
+        const char *short_flag;
+        const char *long_flag;
+        const char *argument;
+        const char *desc;
+};
+
+/*
+ * Define the program info
+ *
+ * flag count, command count, program name, program description, program usage,
+ * ProgramCommands, ProgramFlag
+ */
+typedef struct {
+        int flagc;
+        int cmdc;
+        const char *name;
+        const char *desc;
+        const char *usage;
+        struct ProgramCommands *commands;
+        struct ProgramFlag *flags;
+} ProgramInfo;
+#endif // !PROGRAM_VAR
+
+#ifndef BZ_COMPLETION
+#define BZ_COMPLETION
+/*
+ * Define the program arguments and completions
+ *
+ * argument, completion
+*/
+struct ProgramArguments {
+        const char *name;
+        const char *completions;
+};
+
+/*
+ * Define the environment variables
+ *
+ * name, value
+*/
+struct ProgramEnv {
+        const char *name;
+        const char *value;
+};
+
+/*
+ * Includes the program info and program arguments
+ *
+ * ProgramInfo, ProgramArguments
+*/
+typedef struct {
+        ProgramInfo *info;
+        int argc;
+        int envc;
+        struct ProgramArguments *args;
+        struct ProgramEnv *envs;
+} CompletionInfo;
+#endif // !BZ_COMPLETION
+
+/*
+ * Generates bash completions for commands, flags and arguments
+*/
+void generate_zsh_completion(const CompletionInfo *info);
+#endif // BASHGEN_H
