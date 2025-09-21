687aac3ef80b35df9398ff7273ebd091c8799107
Augusto Coronel <aoc@getgoogleoff.me>
2025-09-21 15:48:22 -0300

    feat(printh): if no options or commands are found, skip

--- info/printh.c
+++ info/printh.c
@@ -108,6 +108,24 @@ int compare_flags(const void *a, const void *b) {
         return strcmp(get_flag_sort_key(flagA), get_flag_sort_key(flagB));
 }
 
+int has_commands(ProgramInfo info) {
+        int exist = 0;
+        for (int i = 0; i < info.cmdc; i++) {
+                if (info.commands->cmd != NULL) exist++;
+        }
+        return exist;
+}
+
+int has_options(ProgramInfo info) {
+        int exist = 0;
+        for (int i = 0; i < info.flagc; i++) {
+                if (info.flags->long_flag != NULL ||
+                    info.flags->short_flag != NULL)
+                        exist++;
+        }
+        return exist;
+}
+
 void printh(ProgramInfo program_info) {
         FILE *out = stdout;
 
@@ -123,71 +141,83 @@ void printh(ProgramInfo program_info) {
         fprintf(out, "  %s%s%s", PH_BOLD, program_info.name, PH_RESET);
         fprintf(out, " %s\n\n", program_info.usage);
 
-        printh_p("Commands:", PH_BOLD_UNDERLINE);
-        fprintf(out, "\n");
+        if (has_commands(program_info) != 0) {
+                printh_p("Commands:", PH_BOLD_UNDERLINE);
+                fprintf(out, "\n");
 
-        for (int i = 0; i < program_info.cmdc; i++) {
-                const char *cmd = program_info.commands[i].cmd;
-                const char *arg = program_info.commands[i].argument;
-                const char *desc = program_info.commands[i].cmd_desc;
+                for (int i = 0; i < program_info.cmdc; i++) {
+                        const char *cmd = program_info.commands[i].cmd;
+                        const char *arg = program_info.commands[i].argument;
+                        const char *desc = program_info.commands[i].cmd_desc;
 
-                char cmd_full[128] = { 0 };
+                        char cmd_full[128] = { 0 };
 
-                if (arg) {
-                        snprintf(cmd_full, sizeof(cmd_full), "%s%s%s %s",
-                                 PH_BOLD, cmd, PH_RESET, arg);
-                } else {
-                        snprintf(cmd_full, sizeof(cmd_full), "%s%s%s", PH_BOLD,
-                                 cmd, PH_RESET);
-                }
-
-                fprintf(out, "  %s\n", cmd_full);
-                if (desc && strlen(desc) > 0) {
-                        print_d(desc);
-                }
-        }
-        fprintf(out, "\n");
-
-        printh_p("Options:", PH_BOLD_UNDERLINE);
-        fprintf(out, "\n");
-
-        for (int i = 0; i < program_info.flagc; i++) {
-                const char *short_flag = program_info.flags[i].short_flag;
-                const char *long_flag = program_info.flags[i].long_flag;
-                const char *arg = program_info.flags[i].argument;
-                const char *desc = program_info.flags[i].desc;
-
-                char flag_buffer[128] = { 0 };
-
-                if (short_flag && long_flag) {
-                        snprintf(flag_buffer, sizeof(flag_buffer),
-                                 "%s%s%s, %s%s%s", PH_BOLD, short_flag,
-                                 PH_RESET, PH_BOLD, long_flag, PH_RESET);
-                } else if (long_flag) {
-                        snprintf(flag_buffer, sizeof(flag_buffer), "%s%s%s",
-                                 PH_BOLD, long_flag, PH_RESET);
-                } else if (short_flag) {
-                        snprintf(flag_buffer, sizeof(flag_buffer), "%s%s%s",
-                                 PH_BOLD, short_flag, PH_RESET);
-                }
-
-                if (arg) {
-                        char upper_arg[32] = { 0 };
-                        for (int j = 0; arg[j] && j < 30; j++) {
-                                upper_arg[j] = (char)toupper(arg[j]);
+                        if (arg) {
+                                snprintf(cmd_full, sizeof(cmd_full),
+                                         "%s%s%s %s", PH_BOLD, cmd, PH_RESET,
+                                         arg);
+                        } else {
+                                snprintf(cmd_full, sizeof(cmd_full), "%s%s%s",
+                                         PH_BOLD, cmd, PH_RESET);
                         }
 
-                        strncat(flag_buffer, " <",
-                                sizeof(flag_buffer) - strlen(flag_buffer) - 1);
-                        strncat(flag_buffer, upper_arg,
-                                sizeof(flag_buffer) - strlen(flag_buffer) - 1);
-                        strncat(flag_buffer, ">",
-                                sizeof(flag_buffer) - strlen(flag_buffer) - 1);
+                        fprintf(out, "  %s\n", cmd_full);
+                        if (desc && strlen(desc) > 0) {
+                                print_d(desc);
+                        }
                 }
+                fprintf(out, "\n");
+        }
 
-                fprintf(out, "  %s\n", flag_buffer);
-                if (desc && strlen(desc) > 0) {
-                        print_d(desc);
+        if (has_options(program_info) != 0) {
+                printh_p("Options:", PH_BOLD_UNDERLINE);
+                fprintf(out, "\n");
+
+                for (int i = 0; i < program_info.flagc; i++) {
+                        const char *short_flag =
+                                program_info.flags[i].short_flag;
+                        const char *long_flag = program_info.flags[i].long_flag;
+                        const char *arg = program_info.flags[i].argument;
+                        const char *desc = program_info.flags[i].desc;
+
+                        char flag_buffer[128] = { 0 };
+
+                        if (short_flag && long_flag) {
+                                snprintf(flag_buffer, sizeof(flag_buffer),
+                                         "%s%s%s, %s%s%s", PH_BOLD, short_flag,
+                                         PH_RESET, PH_BOLD, long_flag,
+                                         PH_RESET);
+                        } else if (long_flag) {
+                                snprintf(flag_buffer, sizeof(flag_buffer),
+                                         "%s%s%s", PH_BOLD, long_flag,
+                                         PH_RESET);
+                        } else if (short_flag) {
+                                snprintf(flag_buffer, sizeof(flag_buffer),
+                                         "%s%s%s", PH_BOLD, short_flag,
+                                         PH_RESET);
+                        }
+
+                        if (arg) {
+                                char upper_arg[32] = { 0 };
+                                for (int j = 0; arg[j] && j < 30; j++) {
+                                        upper_arg[j] = (char)toupper(arg[j]);
+                                }
+
+                                strncat(flag_buffer, " <",
+                                        sizeof(flag_buffer) -
+                                                strlen(flag_buffer) - 1);
+                                strncat(flag_buffer, upper_arg,
+                                        sizeof(flag_buffer) -
+                                                strlen(flag_buffer) - 1);
+                                strncat(flag_buffer, ">",
+                                        sizeof(flag_buffer) -
+                                                strlen(flag_buffer) - 1);
+                        }
+
+                        fprintf(out, "  %s\n", flag_buffer);
+                        if (desc && strlen(desc) > 0) {
+                                print_d(desc);
+                        }
                 }
         }
 }
