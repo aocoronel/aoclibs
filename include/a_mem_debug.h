27d3f758c5ce30f5aa73e9f2c95f323cd288615b
Augusto Coronel <aocoronel@proton.me>
2025-11-27 21:55:55 -0300

    feat(heap): add debugging utility to std memory std functions

---
+++ include/a_mem_debug.h
@@ -0,0 +1,30 @@
+#ifndef AOCLIBS_MEM_DEBUG_H
+#define AOCLIBS_MEM_DEBUG_H
+
+#include <stddef.h>
+
+#define malloc(x) debug_malloc(x, __func__, __FILE__, __LINE__)
+#define calloc(n, x) debug_calloc(n, x, __func__, __FILE__, __LINE__)
+#define realloc(p, x) debug_realloc(p, x, __func__, __FILE__, __LINE__)
+#define free(x) debug_free(x, __func__, __FILE__, __LINE__)
+
+typedef struct MemEntry {
+    void *ptr;
+    size_t size;
+    const char *func;
+    const char *file;
+    int line;
+    struct MemEntry *next;
+} MemEntry;
+
+static MemEntry *head = NULL;
+static int allocCount = 0;
+static int freeCount = 0;
+
+void *debug_malloc(size_t size, const char *func, const char *file, int line);
+void *debug_calloc(size_t nmemb, size_t size, const char *func, const char *file, int line);
+void *debug_realloc(void *ptr, size_t size, const char *func, const char *file, int line);
+void debug_free(void *ptr, const char *func, const char *file, int line);
+void print_memory_summary(void);
+
+#endif
