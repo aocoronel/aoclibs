c12ad452e27ba19dac9035de865cf5202888559e
Augusto Coronel <aocoronel@proton.me>
2025-11-27 22:04:59 -0300

    feat(heap): Nullable string_realloc

--- heap/a_string_realloc.c
+++ heap/a_string_realloc.c
@@ -1,12 +1,13 @@
 #include <a_error_fn_ok.h>
 #include <a_error_macros.h>
+#include <a_heap_fn_alloc.h>
 #include <a_heap_fn_realloc.h>
 #include <a_string_types.h>
 #include <assert.h>
 #include <stddef.h>
 
 Error string_realloc(String *s, usize capacity) {
-        assert(s != NULL);
+        if (s != NULL) try(heap_alloc(&s->heap, capacity));
         try(resize_alloc(&s->heap, capacity));
         return ok();
 }
