ac63c227756b06e09a4031cb82637aacfecb6794
Augusto Coronel <aocoronel@proton.me>
2025-12-08 15:57:49 -0300

    feat(fs): add directory walker and filetype check

---
+++ fs/test.c
@@ -0,0 +1,25 @@
+#include <a_fs_dir_walker.h>
+#include <stdio.h>
+
+void if_null(const char *path) {
+        printf("%s doesn't exist!\n", path);
+}
+
+void if_reg(const char *path) {
+        printf("%s is a file!\n", path);
+}
+
+void if_dir(const char *path) {
+        printf("%s is a dir!\n", path);
+}
+
+void if_link(const char *path) {
+        printf("%s is a link!\n", path);
+}
+
+int main(int argc, char *argv[]) {
+        printf("Hi\n");
+        dir_walker(".", true, if_dir, if_reg, if_link, if_null);
+        dir_walker(".", true, NULL, NULL, NULL, if_null);
+        return 0;
+}
---
+++ include/a_fs_types.h
@@ -0,0 +1,11 @@
+#ifndef AOCLIBS_FS_TYPES_H
+#define AOCLIBS_FS_TYPES_H
+
+enum FileType {
+        F_NULL,
+        F_REGULAR,
+        F_SYMLINK,
+        F_DIR,
+};
+
+#endif
