f38b7770263f8db95d22a94fc4f8b1f66e7c83f4
Augusto Coronel <aocoronel@proton.me>
2025-12-21 16:40:57 -0300

    fix(common): exported defines to external files

--- aoclibs/common.h
+++ aoclibs/common.h
@@ -1,33 +1,10 @@
 #ifndef AOCLIBS_COMMON_H
 #define AOCLIBS_COMMON_H
 
-#ifndef __clang__
-#  define _Nullable
-#  define _Nonnull
-#endif
-
-// === Strings ===
-
-#define string_literal_len(s) ((sizeof((s)) / sizeof((s)[0])) - sizeof((s)[0]))
-#define array_len(a) sizeof(a) / sizeof(a[0])
-
-// === Function Attributes ===
-
-#define COLD_FUNC __attribute__((cold))
-#define CONST_FUNC __attribute__((const))
-#define DEPRECATED(x) __attribute__((deprecated(x)))
-#define HEAP_ALLOCATE __attribute__((malloc))
-#define HOT_FUNC __attribute__((hot))
-#define NONNULL_RETURN __attribute__((returns_nonnull))
-#define PURE_FUNC __attribute__((pure))
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
-#define WARN_UNUSED __attribute__((warn_unused_result))
-#define NORETURN __attribute__((noreturn))
-
-// === Includes ===
-
 #include <aoclibs/assert.h>
+#include <aoclibs/attributes.h>
 #include <aoclibs/error.h>
 #include <aoclibs/int.h>
+#include <aoclibs/macros.h>
 
 #endif
