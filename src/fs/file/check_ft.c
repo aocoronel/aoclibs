0787a2531f51f2a67b133c1e3724015a5aa992f4
Augusto Coronel <aocoronel@proton.me>
2025-12-21 16:42:14 -0300

    feat(check_ft): add assertions

--- src/fs/file/check_ft.c
+++ src/fs/file/check_ft.c
@@ -1,21 +1,24 @@
 #define _XOPEN_SOURCE 700
 
+#include <aoclibs/common.h>
 #include <aoclibs/fs/types.h>
 #include <aoclibs/fs/file.h>
 #include <stdbool.h>
 #include <sys/stat.h>
 
-FileType file_check_ft(const char *path) {
+FileType file_check_ft(const char *_Nonnull path) {
+        ASSERT(path != NULL, "%s", "passing NULL pointer to Nonnull parameter");
+
         struct stat st;
         if (lstat(path, &st) == -1) return F_NULL;
 
         if (S_ISREG(st.st_mode)) return F_REG;
-        if (S_ISDIR(st.st_mode)) return F_DIR;
-        if (S_ISLNK(st.st_mode)) return F_LNK;
-        if (S_ISCHR(st.st_mode)) return F_CHR;
-        if (S_ISBLK(st.st_mode)) return F_BLK;
-        if (S_ISFIFO(st.st_mode)) return F_FIFO;
-        if (S_ISSOCK(st.st_mode)) return F_SOCK;
+        else if (S_ISDIR(st.st_mode)) return F_DIR;
+        else if (S_ISLNK(st.st_mode)) return F_LNK;
+        else if (S_ISCHR(st.st_mode)) return F_CHR;
+        else if (S_ISBLK(st.st_mode)) return F_BLK;
+        else if (S_ISFIFO(st.st_mode)) return F_FIFO;
+        else if (S_ISSOCK(st.st_mode)) return F_SOCK;
 
         return F_NULL;
 }
