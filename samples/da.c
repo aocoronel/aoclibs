cbb8f144aa1c91886cd4e8fbbfda4f9e76e3c4fb
Augusto Coronel <aocoronel@proton.me>
2026-01-31 14:54:13 -0300

    chore(samples): update da.c

--- samples/da.c
+++ samples/da.c
@@ -30,85 +30,83 @@ int main() {
 
         int hello_len = strlen("hello, world!");
 
-        da_append_cstr(&mychar, "acorn", strlen("acorn"));
+        dar_append_cstr(&mychar, "acorn", strlen("acorn"));
+        // or
+        dar_append_cstrl(&mychar, "acorn");
 
-        printf("last: %s\n", (char *)da_last(&mychar));
+        printf("last: %s\n", (char *)dar_last(&mychar));
 
-        da_append_cstr(&mychar, "berlim", strlen("berlim"));
+        dar_append_cstr(&mychar, "berlim", strlen("berlim"));
 
-        printf("last: %s\n", (char *)da_last(&mychar));
+        printf("last: %s\n", (char *)dar_last(&mychar));
 
-        da_append_cstr(&mychar, "Ball", strlen("Ball"));
+        dar_append_cstr(&mychar, "Ball", strlen("Ball"));
 
-        printf("last: %s\n", (char *)da_last(&mychar));
+        printf("last: %s\n", (char *)dar_last(&mychar));
 
-        da_append_cstr(&mychar, "Tripto Tripto Tripto Tripto",
-                       strlen("Tripto Tripto Tripto Tripto"));
+        dar_append_cstr(&mychar, "Tripto Tripto Tripto Tripto",
+                        strlen("Tripto Tripto Tripto Tripto"));
 
-        printf("last: %s\n", (char *)da_last(&mychar));
+        printf("last: %s\n", (char *)dar_last(&mychar));
 
         const char *strings[] = { "hi", "hi2", "hi3" };
-        _da_append_many_cstr(realloc, &mychar, strings, 3);
+        dar_append_many_cstr(&mychar, strings, 3);
 
-        char *str = (char *)da_last(&mychar);
+        char *str = (char *)dar_last(&mychar);
         str[0] = 'b'; // hi3 is allocated in the heap
 
         for (int i = 0; i < mychar.len; i++) {
-                printf("%s\n", (char *)da_get(&mychar, i));
+                printf("%s\n", (char *)dar_get(&mychar, i));
         }
 
         DynamicArena myerrors = { 0 };
         myerrors.arena = &arena;
-        _da_append_many(realloc, &myerrors, errors, 3, sizeof(ErrorInfo), _Alignof(ErrorInfo));
-        printf("%d\n", ((ErrorInfo *)da_get(&myerrors, 1))->err_code); // prints -2
+        dar_append_many(&myerrors, errors, 3, sizeof(ErrorInfo), _Alignof(ErrorInfo));
+        printf("%d\n", ((ErrorInfo *)dar_get(&myerrors, 1))->err_code); // prints -2
         printf("myerrors count: %zu\n", myerrors.len);
-        ErrorInfo *myerror = (ErrorInfo *)da_pop(&myerrors);
+        ErrorInfo *myerror = (ErrorInfo *)dar_pop(&myerrors);
         printf("Value returned from pop: %d\n", myerror->err_code);
         printf("Current myerror: last value is %d and index is %zu\n",
-               ((ErrorInfo *)da_last(&myerrors))->err_code, myerrors.len);
+               ((ErrorInfo *)dar_last(&myerrors))->err_code, myerrors.len);
 
-        da_foreach(&myerrors, i) {
-                ErrorInfo *x = (ErrorInfo *)da_get(&myerrors, i);
+        foreach(&myerrors, i) {
+                ErrorInfo *x = (ErrorInfo *)dar_get(&myerrors, i);
                 printf("Error Code (Index %zu) : %d\n", i, x->err_code);
         };
 
-        DynamicArena myerrors2 = { 0 };
-
-        da_clone(&myerrors2, &myerrors);
-
         printf("Before squizzle\n");
-        printf("w = %d\n", ((ErrorInfo *)da_get(&myerrors, 0))->err_code); // -1
-        printf("z = %d\n", ((ErrorInfo *)da_get(&myerrors, 1))->err_code); // -2
+        printf("w = %d\n", ((ErrorInfo *)dar_get(&myerrors, 0))->err_code); // -1
+        printf("z = %d\n", ((ErrorInfo *)dar_get(&myerrors, 1))->err_code); // -2
 
-        da_swap(&myerrors, 0, 1);
+        dar_swap(&myerrors, 0, 1);
 
         printf("Squizzled:\n");
-        printf("w = %d\n", ((ErrorInfo *)da_get(&myerrors, 0))->err_code); // -2
-        printf("z = %d\n", ((ErrorInfo *)da_get(&myerrors, 1))->err_code); // -1
+        printf("w = %d\n", ((ErrorInfo *)dar_get(&myerrors, 0))->err_code); // -2
+        printf("z = %d\n", ((ErrorInfo *)dar_get(&myerrors, 1))->err_code); // -1
 
-        printf("myerror = %d\n", ((ErrorInfo *)da_get(&myerrors, 0))->err_code);
+        printf("myerror = %d\n", ((ErrorInfo *)dar_get(&myerrors, 0))->err_code);
 
         DynamicArena int_da = { .arena = &arena };
         int a = 5, b = 2, c = 8, d = 1, e = 3;
 
-        da_append(&int_da, &a, sizeof(int));
-        da_append(&int_da, &b, sizeof(int));
-        da_append(&int_da, &c, sizeof(int));
-        da_append(&int_da, &d, sizeof(int));
-        da_append(&int_da, &e, sizeof(int));
+        dar_append(&int_da, &a, sizeof(int));
+        dar_append(&int_da, &b, sizeof(int));
+        dar_append(&int_da, &c, sizeof(int));
+        dar_append(&int_da, &d, sizeof(int));
+        dar_append(&int_da, &e, sizeof(int));
 
-        int *i = (int *)da_get(&int_da, 0);
+        int *i = (int *)dar_get(&int_da, 0);
         printf("%d\n", *i);
 
-        da_set(int *, &int_da, 0, b);
+        dar_set(int *, &int_da, 0, b);
         printf("%d\n", *i);
 
         for (size_t i = 0; i < int_da.len; i++) {
-                printf("%d ", *(int *)da_get(&int_da, i));
+                printf("%d ", *(int *)dar_get(&int_da, i));
         }
         // Output: 2 2 8 1 3
 
-        da_free(&int_da);
+        dar_free(&int_da);
 
         // Notice how just a single arena was used.
         // It was used to allocate strings, structs and ints.
