1551c97fb919d54ea299ed7eab6252ae6d785882
Augusto Coronel <aocoronel@proton.me>
2026-02-03 16:06:03 -0300

    refactor(da): add support for stack allocated buffers, and some new macros

--- base/da.h
+++ base/da.h
@@ -33,9 +33,14 @@
 // Except for some like the very following ones, which are internal
 
 // Convenient assertions to prevent access out of bounds
-#define _aoc_assert_da_is_valid(da) (assert((da)->len > 0 && "out of bounds access"))
+#define _aoc_assert_da_index_is_valid(da) (assert((da)->len > 0 && "out of bounds access"))
+#define _aoc_assert_da_is_not_null(da) (assert((da)->data != NULL && "cannot modify NULL pointer"))
+#define _aoc_assert_da_data_is_not_null(da) \
+        (assert((da)->data != NULL && "cannot modify NULL pointer"))
 
-// Before any attempt to add new values to the DA, always reserve the memory.
+// If you know before hand you need a big dynamic array, you can reserve memory.
+//
+// The new capacity is already calculates the size of the type.
 //
 // DynamicArray my_da = { 0 };
 // da_reserve(&my_da, (&my_da)->len + 1); // Needs to allocate one value
@@ -64,26 +69,38 @@
         } while (0)
 
 // Returns last pointer
-#define aoc_da_last(da) (da)->data[_aoc_assert_da_is_valid(da), (da)->len - 1]
+#define aoc_da_last(da) (da)->data[_aoc_assert_da_index_is_valid(da), (da)->len - 1]
 
 // Returns last pointer, and removes it from the DA
 #define aoc_da_pop(da) \
-        (da)->data[(assert((da)->len < (da)->cap && "out of bounds access")), --(da)->len]
+        (da)->data[(assert((da)->len < (da)->cap && "buffer underflow")), --(da)->len]
 
-// Empties the DA
 #define aoc_da_reset(da) (da)->len = 0
+
+#define aoc_da_erase(da) memset((da)->data, 0, (da)->cap)
+
+// Removes an item from the dynamic array from an index
+#define aoc_da_drop(da, index)                                                              \
+        do {                                                                                \
+                _aoc_assert_da_is_not_null(da);                                             \
+                assert((da)->len > (index);     \
+                memmove((da)->data[(index)], (da)->data[(index) + 1], (da)->len - (index)); \
+                (da)->len--; \
+                (da)->data[(da)->len] = 0;                                                  \
+        } while (0)
 
 // Append any type. For C strings, look for the aoc_da_append_many_cstr
 #define aoc_da_append(da, data) _aoc_da_append(AOCLIBS_DA_REALLOC, (da), (data))
 #define _aoc_da_append(realloc, da, item)                        \
         do {                                                     \
+                _aoc_assert_da_is_not_null(da);                  \
                 _aoc_da_reserve((realloc), (da), (da)->len + 1); \
                 (da)->data[(da)->len++] = (item);                \
         } while (0)
 
 // This macro doesn't include the null terminator, and must be added manually
-#define aoc_da_append_cstr(da, items_buff, items_size) \
-        _aoc_da_append_many(AOCLIBS_DA_REALLOC, da, items_buff, cstrlen((items_buff), (items_size)))
+#define aoc_da_append_cstr(da, items_buff, items_len) \
+        _aoc_da_append_many(AOCLIBS_DA_REALLOC, da, items_buff, items_len)
 
 // Adding strings using this macro will include the null terminator if the
 // string buffer size is passed as items_size
@@ -92,6 +109,7 @@
 
 #define _aoc_da_append_many(realloc, da, items_buff, items_size)                                  \
         do {                                                                                      \
+                _aoc_assert_da_is_not_null(da);                                                   \
                 _aoc_da_reserve(realloc, (da), (da)->len + (items_size));                         \
                 memcpy((da)->data + (da)->len, (items_buff), (items_size) * sizeof(*(da)->data)); \
                 (da)->len += (items_size);                                                        \
@@ -101,14 +119,61 @@
 #define _aoc_da_append_null(realloc, sb) _aoc_da_append_many(realloc, sb, "", 1)
 
 #define aoc_da_clone(dest, src) _aoc_da_clone(AOCLIBS_DA_REALLOC, dest, src)
-#define _aoc_da_clone(realloc, dest, src)           \
-        _aoc_da_reserve(realloc, dest, (src)->cap); \
-        (dest)->len = (src)->len;                   \
-        memcpy((dest)->data, (src)->data, (src)->len * sizeof(void *));
+#define _aoc_da_clone(realloc, dest, src)                                       \
+        do {                                                                    \
+                _aoc_assert_da_is_not_null(da);                                 \
+                _aoc_da_reserve(realloc, dest, (src)->cap);                     \
+                (dest)->len = (src)->len;                                       \
+                memcpy((dest)->data, (src)->data, (src)->len * sizeof(void *)); \
+        } while (0)
 
 #define aoc_da_swap(Type, da, i1, i2) aoc_swap(Type, (da)->data[i1], (da)->data[i2])
 
 #define aoc_da_sort(da, fn) qsort((da)->data, (da)->len, sizeof((da)->data[0]), fn)
+
+// Case a dynamic array is used with a buffer allocated in the stack, the following
+// implementation can be used: Dynamic Array Stack (das)
+//
+// This is slighly different, because the dynamic array cannot be automatically resized.
+// So let the user handle in case the append operation would truncate the buffer.
+//
+// The usage follows something like:
+//
+// if das_append(my_da, 1); // do nothing if fails
+//
+// OR
+//
+// if das_append(my_da, 1)
+// else handle_error;
+//
+// These macros don't expect the da->data to be NULL.
+
+#define aoc_das_append _aoc_das_append
+#define _aoc_das_append(da, item)                    \
+        ((da)->len + 1 > (da)->cap) {                \
+                _aoc_assert_da_is_not_null(da);      \
+                _aoc_assert_da_data_is_not_null(da); \
+                (da)->data[(da)->len++] = (item);    \
+        }
+
+#define aoc_das_append_cstr(da, cstr, cstr_len) _aoc_das_append_many(da, cstr, cstr_len)
+
+#define aoc_das_append_many _aoc_das_append_many
+
+#define _aoc_das_append_many(da, items_buff, items_size)                                          \
+        ((da)->len + items_size > (da)->cap) {                                                    \
+                _aoc_assert_da_is_not_null(da);                                                   \
+                _aoc_assert_da_data_is_not_null(da);                                              \
+                memcpy((da)->data + (da)->len, (items_buff), (items_size) * sizeof(*(da)->data)); \
+                (da)->len += (items_size);                                                        \
+        }
+
+#define aoc_das_append_null(da)               \
+        ((da)->len + 1 > (da)->cap) {         \
+                (da)->data[(da)->len] = '\0'; \
+        }
+
+#define aoc_da_is_null(da) !(da) || !(da)->data
 
 #ifdef AOCLIBS_STRIP_PREFIX
 #define _da_append _aoc_da_append
@@ -128,10 +193,19 @@
 #define da_reserve aoc_da_reserve
 
 #define da_clear aoc_da_clear
+#define da_erase aoc_da_erase
+#define da_drop aoc_da_drop
 #define da_last aoc_da_last
 #define da_pop aoc_da_pop
 #define da_sort aoc_da_sort
 #define da_swap aoc_da_swap
+
+#define das_append aoc_das_append
+#define das_append_many aoc_das_append_many
+#define das_append_cstr aoc_das_append_cstr
+#define das_append_null aoc_das_append_null
+
+#define da_is_null aoc_da_is_null
 #endif
 
 #endif
