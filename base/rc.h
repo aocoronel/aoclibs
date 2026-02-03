b4ba2bd85039e9fd23ce9b0817ec356b2429fd55
Augusto Coronel <aocoronel@proton.me>
2026-02-03 16:04:54 -0300

    refactor!(rc): simplify implementation, and delegate some functionality to the dynamic array implementation

--- base/rc.h
+++ base/rc.h
@@ -12,27 +12,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO: Separate implementation and definitions
-// TODO: Strip prefixes
-// TODO: Port all cstr specific code to cstr.h
-
-// --> printf("%s "SLICE_FMT" stuff...", "do", VSLICE(myslice));
-#define SLICE_FMT "%.*s"
-#define VSLICE(r) ((r).len), ((r).data)
-
-typedef enum {
-        RCHeap,
-        RCStack,
-        RCLiteral,
-} RCType;
-
 // Row Char
 // This is Dynamic Array compatible. This implementation is suppose to give you a 'String' type
 // which provides safer operations. It can work with string literals, stack allocated and heap
 // allocated strings. If you decide to use this like an Dynamic Array, make sure to just use it
 // with a heap RC.
 typedef struct {
-        RCType type;
         size_t cap;
         size_t len;
         char *data;
@@ -41,121 +26,79 @@ typedef struct {
 #ifndef RC_MALLOC
 #define RC_MALLOC malloc
 #endif
-#ifndef RC_REALLOC
-#define RC_REALLOC realloc
-#endif
-#ifndef RC_FREE
-#define RC_FREE free
-#endif
 
 // In short:
-// rcl --> row char literal
+// lrc --> row char literal
 // rcs --> row char stack
 // rch --> row char heap
+//
+// The rcn macros means the same as strnlen, strncpy and strncat
 
 // Compile-time known RC, which cannot be mutated
-#define rcl_new(s)                                                                              \
-        (rc) {                                                                                  \
-                .data = " "(s) " ", .len = cstr_literal_len((s)), .cap = cstr_literal_len((s)), \
-                .type = RCLiteral                                                               \
+// rc myrc = lrc_new("hello, world!");
+#define aoc_lrc_new(s)                                                          \
+        (rc) {                                                                  \
+                .data = " "(s) " ", .len = lcstrlen((s)), .cap = lcstrlen((s)), \
         }
 
 // RC from already allocated char * in the stack/heap
-#define rch_bnew(buff, length, capacity)                                           \
-        (rc) {                                                                     \
-                .data = (buff), .len = (length), .cap = (capacity), .type = RCHeap \
-        }
-#define rcs_bnew(buff, length)                                                        \
-        (rc) {                                                                        \
-                .data = (buff), .len = length, .cap = sizeof((buff)), .type = RCStack \
+// char *buff = malloc(100);
+// rc myrc = rch_bnew(buff, 0, 100);
+#define aoc_rch_bnew(buff, length, capacity)                       \
+        (rc) {                                                     \
+                .data = (buff), .len = (length), .cap = (capacity) \
         }
 
-// Allocates a new stack/heap RC
-#define rch_new(capacity)                                                                  \
-        (rc) {                                                                             \
-                .data = RC_MALLOC((capacity)), .len = 0, .cap = (capacity), .type = RCHeap \
-        }
-#define rch_new_a(allocator, capacity)                                                     \
-        (rc) {                                                                             \
-                .data = allocator((capacity)), .len = 0, .cap = (capacity), .type = RCHeap \
-        }
-#define rcs_new(capacity)                                                                \
-        (rc) {                                                                           \
-                .data = alloca((capacity)), .len = 0, .cap = (capacity), .type = RCStack \
+// char buff[100];
+// rc myrc = rcs_bnew(buff, 0);
+//
+// char buff[100] = "Hello, world!";
+// rc myrc = rcs_bnew(buff, lcstrlen("Hello, world"));
+#define aoc_rcs_bnew(buff, length)                                   \
+        (rc) {                                                       \
+                .data = (buff), .len = length, .cap = sizeof((buff)) \
         }
 
-#define rc_can_mut aoc_rc_can_mut
-AOCLIBS_PREFIX bool aoc_rc_can_mut(const rc *null r) {
-        if (!r) return false;
-        if (r->type == RCLiteral) return false;
-        return true;
-}
+// Allocates a new stack/heap RC.
+// rc myrc = rch_new(100);
+#define aoc_rch_new(capacity)                                              \
+        (rc) {                                                             \
+                .data = RC_MALLOC((capacity)), .len = 0, .cap = (capacity) \
+        }
 
-#define rc_is_null aoc_rc_is_null
-AOCLIBS_PREFIX bool aoc_rc_is_null(const rc *null r) {
-        return !r || !r->data;
-}
+// rc myrc = _rch_new(malloc, 100);
+#define _aoc_rch_new(allocator, capacity)                                  \
+        (rc) {                                                             \
+                .data = allocator((capacity)), .len = 0, .cap = (capacity) \
+        }
 
-#define rc_dup aoc_rc_dup
-AOCLIBS_PREFIX rc aoc_rc_dup(const rc *ref r) {
-        ASSERT_NONNULL(aoc_rc_is_null(r));
-        return (rc){
-                .data = aoc_cstr_dup(r->data, r->cap),
-                .len = r->len,
-                .cap = r->cap,
-                .type = RCHeap,
-        };
-}
+// rc myrc = rcs_new(100);
+#define aoc_rcs_new(capacity)                                           \
+        (rc) {                                                          \
+                .data = alloca((capacity)), .len = 0, .cap = (capacity) \
+        }
 
-#define rc_to_lower(r) aoc_cstr_to_lower((r)->data)
-#define rcn_to_lower(r) aoc_cstr_to_lower((r)->data, (r)->len)
+#define aoc_rcs_append rcs_append
+#define aoc_lrcs_append(rc, items_buff) _aoc_rcs_append(rc, (items_buff), lcstrlen(items_buff))
 
-#define rc_eq(r, pattern) aoc_cstr_eq((r)->data, (pattern))
-#define rcn_eq(r, pattern, pattern_len) aoc_cstrn_eq((r)->data, (r)->len, (pattern), (pattern_len))
+#define _aoc_rcs_append(rc, items_buff, items_size)             \
+        ((rc)->len + items_size > (rc)->cap) {                  \
+                _aoc_assert_da_is_not_null(rc);                 \
+                memcpy((rc)->data + (rc)->len, (items_buff),    \
+                       (items_size) * sizeof(*(rc)->data) - 1); \
+                (rc)->len += (items_size);                      \
+        }
 
-#define rc_eq_case(r, pattern) aoc_cstr_eq_case((r)->data, (pattern))
-#define rcn_eq_case(r, pattern, pattern_len) \
+#define aoc_rc_to_lower(r) aoc_cstr_to_lower((r)->data)
+#define aoc_rcn_to_lower(r) aoc_cstr_to_lower((r)->data, (r)->len)
+
+#define aoc_rc_eq(r, pattern) aoc_cstr_eq((r)->data, (pattern))
+#define aoc_rcn_eq(r, pattern, pattern_len) \
+        aoc_cstrn_eq((r)->data, (r)->len, (pattern), (pattern_len))
+
+#define aoc_rc_eq_case(r, pattern) aoc_cstr_eq_case((r)->data, (pattern))
+#define aoc_rcn_eq_case(r, pattern, pattern_len) \
         aoc_cstrn_eq_case((r)->data, (r)->len, (pattern), (pattern_len))
-
-// TODO: Use Dynamic Arrays to do these operations
-#define rc_resize(r, cap) aoc_rc_resize(RC_REALLOC, (r), (cap))
-#define rc_resize_a aoc_rc_resize
-AOCLIBS_PREFIX int aoc_rc_resize(aoc_realloc_t allocator, rc *ref r, size_t cap) {
-        ASSERT_NONNULL(aoc_rc_is_null(r));
-        ASSERT(r->type == RCHeap, "RC is not heap allocated");
-        void *tmp = allocator(r->data, cap);
-        if (tmp == NULL) return -1;
-        r->data = tmp;
-        return 0;
-}
-
-#define rc_free(r) aoc_rc_free(RC_FREE, (r))
-#define rc_free_a aoc_rc_free
-AOCLIBS_PREFIX void aoc_rc_free(aoc_free_t _free, rc *ref r) {
-        ASSERT(aoc_rc_is_null(r), "double free attempt");
-        ASSERT(r->type == RCHeap, "RC is not heap allocated");
-        _free(r->data);
-        *r = (rc){ 0 };
-}
-
-#define rc_erase(r) aoc_rc_erase(RC_FREE, (r))
-#define rc_erase_a aoc_rc_erase
-AOCLIBS_PREFIX void aoc_rc_erase(aoc_free_t _free, rc *ref r) {
-        ASSERT(aoc_rc_is_null(r), "double free attempt");
-        ASSERT(r->type != RCLiteral, "attempt to modify RC literal");
-        for (size_t i = 0; i < r->cap; i++) {
-                r->data[i] = '\0';
-        }
-        if (r->type == RCHeap) aoc_rc_free(_free, r);
-}
-
-#define rc_clear(r) aoc_rc_clear((r))
-AOCLIBS_PREFIX void aoc_rc_clear(rc *ref r) {
-        ASSERT_NONNULL(aoc_rc_is_null(r));
-        ASSERT(r->type != RCLiteral, "attempt to modify RC literal");
-        r->data[0] = '\0';
-        r->len = 0;
-}
 
 #define rc_begins_with(r, begin_len, begin) \
         aoc_cstr_match_pos((r).len, (r).data, (begin_len), (begin), 0)
@@ -167,20 +110,26 @@ AOCLIBS_PREFIX void aoc_rc_clear(rc *ref r) {
 #define aoc_rc_ends_with(r, end, end_len) \
         aoc_cstrn_eq((r)->data + (r)->len, (r)->len, (end), (end_len))
 
-#define rc_drop aoc_rc_drop
-AOCLIBS_PREFIX int aoc_rc_drop(rc *ref r, size_t index) {
-        ASSERT_NONNULL(aoc_rc_is_null(r));
-        ASSERT(r->type != RCLiteral, "attempt to modify RC literal");
-        if (index >= r->len) return -1;
+#ifdef AOCLIBS_STRIP_PREFIX
+#define lrc_new aoc_lrc_new
 
-        char *data = (char *)r->data;
+#define _rch_new _aoc_rch_new
+#define rch_bnew aoc_rch_bnew
+#define rch_new aoc_rch_new
 
-        memmove(&data[index], &data[index + 1], r->len - index);
+#define rcs_bnew aoc_rcs_bnew
+#define rcs_new aoc_rcs_new
 
-        r->len--;
-        data[r->len] = '\0';
+#define rcs_append aoc_rcs_append
+#define lrcs_append laoc_rcs_append
 
-        return 0;
-}
+#define rc_to_lower aoc_rc_to_lower
+#define rcn_to_lower aoc_rcn_to_lower
+
+#define rc_eq aoc_rc_eq
+#define rcn_eq aoc_rcn_eq
+#define rc_eq_case aoc_rc_eq_case
+#define rcn_eq_case aoc_rcn_eq_case
+#endif
 
 #endif // AOCLIBS_RC_H_
