2ab30a60026e958db67c63f5691920331febfaac
Augusto Coronel <aocoronel@proton.me>
2026-03-29 17:00:57 -0300

    fix(crown): no argument defined causes segfault

--- libs/crown.h
+++ libs/crown.h
@@ -884,7 +884,7 @@ fn int crown_getopt(CrownCommand *null cmds, char *argv[], int argc) {
         const char *long_opt = opt[i].long_opt;
         const char *short_opt = opt[i].short_opt;
         const size_t flag_arg_idx = opt[i].args;
-        const char *flag_arg = Program->args->data[flag_arg_idx].name;
+        const char *flag_arg = Program->args && Program->args->data != NULL ? Program->args->data[flag_arg_idx].name : NULL;
 
         if (long_opt != NULL && cstr_eq(arg, long_opt)) {
             if (flag_arg != NULL) {
@@ -915,7 +915,7 @@ fn int crown_getcmd(CrownCommand *null cmds, char *argv[], int argc) {
     for (size_t i = 0; i < len; i++) {
         const char *cmd = opt[i].name;
         const size_t cmd_arg_idx = opt[i].args;
-        const char *cmd_arg = Program->args->data[cmd_arg_idx].name;
+        const char *cmd_arg = Program->args && Program->args->data != NULL ? Program->args->data[cmd_arg_idx].name : NULL;
         const CrownCommand *cmd_subcmd =
                 opt[i].subcmd && opt[i].subcmd->data != NULL ? opt[i].subcmd->data : NULL;
 
