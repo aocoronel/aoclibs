40365f0e3f6dd83388ecf21005a5c02e79193cbb
Augusto Coronel <aocoronel@proton.me>
2026-04-12 15:02:11 -0300

    feat(fquery): "regex" simple approach based on functions

---
+++ include/fquery.h
@@ -0,0 +1,66 @@
+#ifndef AOCLIBS_FQUERY_H_
+#define AOCLIBS_FQUERY_H_
+
+#include "base.h"
+#include <stdbool.h>
+
+// FQuery takes the same use case of regular expressions, however, without compilation.
+//
+// Setting up FQuery is simple:
+//
+// int skip_to_a = 'a';
+// const fquery_t *query =
+//      new_fquery(
+//       { .fn = fquery_any, .arg = NULL },
+//       { .fn = fquery_any, .arg = NULL },
+//       { .fn = fquery_skip_to_int, .arg = &skip_to_a });
+// fquery(query, "00adasd")
+
+#define new_fquery(...)   \
+    (fquery_t[]) {        \
+        __VA_ARGS__, NULL \
+    }
+
+typedef const char *(*fquery_fn)(const char *null, const void *);
+
+typedef struct {
+    fquery_fn fn;
+    const void *arg;
+} fquery_t;
+
+// *p == c
+AOCDEF const char *null fquery_eq_int(const char *null p, const void *c) ATTR_PURE;
+
+// regex: "*.c"
+AOCDEF const char *null fquery_skip_to_int(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_skip_to_fn(const char *null p, const void *c) ATTR_PURE;
+
+// regex: "?"
+AOCDEF const char *null fquery_optional(const char *null p, const void *c) ATTR_PURE;
+
+// regex: "."
+AOCDEF const char *null fquery_any(const char *null p, const void *c) ATTR_PURE;
+
+// Wrappers to ctype.h
+AOCDEF const char *null fquery_isalnum(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isalpha(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_iscntrl(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isdigit(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isgraph(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_islower(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isprint(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_ispunct(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isspace(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isupper(const char *null p, const void *c) ATTR_PURE;
+AOCDEF const char *null fquery_isxdigit(const char *null p, const void *c) ATTR_PURE;
+
+AOCDEF bool fquery(const fquery_t *qms, const char *pattern) ATTR_PURE;
+
+// === Internal ===
+AOCDEF bool _fquery(const fquery_t *qms, const char *null p) ATTR_PURE;
+
+#ifdef AOCLIBS_IMPLEMENTATION
+#include "fquery.c"
+#endif // AOCLIBS_IMPLEMENTATION
+
+#endif
---
+++ src/fquery.c
@@ -0,0 +1,201 @@
+#pragma once
+
+#include "base.h"
+#include "fquery.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+AOCDEF const char *null fquery_eq_int(const char *null p, const void *c) {
+    ASSERT_NONNULL(c);
+
+    int _c = *(int *)c;
+    if (!p || *p != _c) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_skip_to_fn(const char *null p, const void *c) {
+    ASSERT_NONNULL(c);
+
+    int (*fn)(int) = (int (*)(int))c;
+    const char *tmp = p;
+    while (tmp && fn(*tmp) == 0) {
+        tmp++;
+    }
+    tmp++;
+    return tmp;
+}
+
+AOCDEF const char *null fquery_skip_to_int(const char *null p, const void *c) {
+    ASSERT_NONNULL(c);
+
+    int _c = *(int *)c;
+    return memchr(p, _c, 1);
+}
+
+AOCDEF const char *null fquery_optional(const char *null p, const void *c) {
+    ASSERT_NONNULL(c);
+    if (!p) return NULL;
+
+    int _c = *(int *)c;
+    if (*p != _c) {
+        return p;
+    }
+    return p + 1;
+}
+
+// regex: "."
+AOCDEF const char *null fquery_any(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || *p == '\0') {
+        return NULL;
+    }
+    return p + 1;
+}
+
+// regex: "[0-9]"
+AOCDEF const char *null fquery_isdigit(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isdigit((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isalnum(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isalnum((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isspace(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isspace((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_iscntrl(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !iscntrl((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_islower(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !islower((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isgraph(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isgraph((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isprint(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isprint((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_ispunct(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !ispunct((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isxdigit(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isxdigit((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isupper(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isupper((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF const char *null fquery_isalpha(const char *null p, const void *c) {
+    DISCARD(c);
+
+    if (!p || !isalpha((unsigned char)*p)) {
+        return NULL;
+    }
+    return p + 1;
+}
+
+AOCDEF bool _fquery(const fquery_t *qms, const char *null p) {
+    if (qms->fn == NULL) return true;
+
+    const fquery_fn fn = qms->fn;
+    const void *arg = qms->arg;
+
+    const char *next = fn(p, arg);
+
+    if (!next) return false;
+
+    return _fquery(qms + 1, next);
+}
+
+AOCDEF bool fquery(const fquery_t *qms, const char *pattern) {
+    ASSERT_NONNULL(qms);
+    ASSERT_NONNULL(pattern);
+    return _fquery(qms, pattern);
+}
+
+// int main(void) {
+//     int skip_to_a = 'a';
+//     int optional_dash = 'b';
+//     const fquery_t *qms = new_fquery({ .fn = fquery_skip_to_fn, .arg = isdigit },
+//                                      { .fn = fquery_optional, .arg = &optional_dash },
+//                                      { .fn = fquery_skip_to_int, .arg = &skip_to_a });
+//
+//     printf("Query: 'any, any, skip_to_a'\n");
+//     printf("  Matching '1ba': Found: %s\n", fquery(qms, "1ba") ? "true" : "false");
+//     printf("  Matching 'bba': Found: %s\n", fquery(qms, "bba") ? "true" : "false");
+//     printf("  Matching 'bb': Found: %s\n", fquery(qms, "bb") ? "true" : "false");
+//     printf("  Matching 'a': Found: %s\n", fquery(qms, "a") ? "true" : "false");
+//     printf("  Matching 'bbbc': Found: %s\n", fquery(qms, "bbbc") ? "true" : "false");
+//
+//     const fquery_t *qms2 = new_fquery(
+//             { .fn = fquery_isdigit }, { .fn = fquery_isdigit }, { .fn = fquery_isdigit });
+//
+//     printf("\nQuery: 'isdigit, isdigit, isdigit'\n");
+//     printf("  Matching '123': Found: %s\n", fquery(qms2, "123") ? "true" : "false");
+//     printf("  Matching '12a': Found: %s\n", fquery(qms2, "12a") ? "true" : "false");
+//
+//     return 0;
+// }
