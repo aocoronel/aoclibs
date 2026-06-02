a8550fb425d98cb3a9cf853e8f77b38da9e9ecf6
Augusto Coronel <aocoronel@proton.me>
2026-06-02 14:32:37 -0300

    refactor(printfc): make level messages lowercase, and have ':' instead of being surrounded by brackets. The reason is "[DEBUG]" is too verbose, compared to "debug:".

--- include/printfc.h
+++ include/printfc.h
@@ -22,17 +22,17 @@
 #define printfc_debug(...) ((void)0)
 #define printfc_trace(...) ((void)0)
 #else // NDEBUG
-#define printfc_debug(...) printfc(COLOR_CYAN, "DEBUG", __VA_ARGS__)
-#define printfc_trace(...) printfc(COLOR_MAGENTA, "TRACE", __VA_ARGS__)
+#define printfc_debug(...) printfc(COLOR_CYAN, "debug", __VA_ARGS__)
+#define printfc_trace(...) printfc(COLOR_MAGENTA, "trace", __VA_ARGS__)
 #endif // NDEBUG
 
 #define printfc(color, level, ...) \
-    (fprintf(stderr, "%s[%s]%s ", color, level, COLOR_RESET), fprintf(stderr, __VA_ARGS__))
-#define printfc_fatal(...) printfc(COLOR_RED, "FATAL", __VA_ARGS__)
-#define printfc_error(...) printfc(COLOR_RED, "ERROR", __VA_ARGS__)
-#define printfc_warn(...) printfc(COLOR_YELLOW, "WARNING", __VA_ARGS__)
-#define printfc_info(...) printfc(COLOR_GREEN, "INFO", __VA_ARGS__)
-#define printfc_critical(...) printfc(COLOR_RED, "CRITICAL", __VA_ARGS__)
+    (fprintf(stderr, "%s%s:%s ", color, level, COLOR_RESET), fprintf(stderr, __VA_ARGS__))
+#define printfc_fatal(...) printfc(COLOR_RED, "fatal", __VA_ARGS__)
+#define printfc_error(...) printfc(COLOR_RED, "error", __VA_ARGS__)
+#define printfc_warn(...) printfc(COLOR_YELLOW, "warning", __VA_ARGS__)
+#define printfc_info(...) printfc(COLOR_GREEN, "info", __VA_ARGS__)
+#define printfc_critical(...) printfc(COLOR_RED, "critical", __VA_ARGS__)
 
 // TTY printfc
 // When the program may not be guaranteed to run in a TTY, use tprintfc instead
@@ -42,19 +42,19 @@
 #define tprintfc_debug(istty, ...) ((void)0)
 #define tprintfc_trace(istty, ...) ((void)0)
 #else // NDEBUG
-#define tprintfc_debug(istty, ...) tprintfc(istty, COLOR_CYAN, "DEBUG", __VA_ARGS__)
-#define tprintfc_trace(istty, ...) tprintfc(istty, COLOR_MAGENTA, "TRACE", __VA_ARGS__)
+#define tprintfc_debug(istty, ...) tprintfc(istty, COLOR_CYAN, "debug", __VA_ARGS__)
+#define tprintfc_trace(istty, ...) tprintfc(istty, COLOR_MAGENTA, "trace", __VA_ARGS__)
 #endif // NDEBUG
 
 #define tprintfc(istty, color, level, ...)                                         \
     do {                                                                           \
         (istty) ? printfc(color, level, __VA_ARGS__) :                             \
-                  (fprintf(stderr, "[%s] ", level), fprintf(stderr, __VA_ARGS__)); \
+                  (fprintf(stderr, "%s: ", level), fprintf(stderr, __VA_ARGS__)); \
     } while (0)
-#define tprintfc_fatal(istty, ...) tprintfc(istty, COLOR_RED, "FATAL", __VA_ARGS__)
-#define tprintfc_error(istty, ...) tprintfc(istty, COLOR_RED, "ERROR", __VA_ARGS__)
-#define tprintfc_warn(istty, ...) tprintfc(istty, COLOR_YELLOW, "WARNING", __VA_ARGS__)
-#define tprintfc_info(istty, ...) tprintfc(istty, COLOR_GREEN, "INFO", __VA_ARGS__)
-#define tprintfc_critical(istty, ...) tprintfc(istty, COLOR_RED, "CRITICAL", __VA_ARGS__)
+#define tprintfc_fatal(istty, ...) tprintfc(istty, COLOR_RED, "fatal", __VA_ARGS__)
+#define tprintfc_error(istty, ...) tprintfc(istty, COLOR_RED, "error", __VA_ARGS__)
+#define tprintfc_warn(istty, ...) tprintfc(istty, COLOR_YELLOW, "warning", __VA_ARGS__)
+#define tprintfc_info(istty, ...) tprintfc(istty, COLOR_GREEN, "info", __VA_ARGS__)
+#define tprintfc_critical(istty, ...) tprintfc(istty, COLOR_RED, "critical", __VA_ARGS__)
 
 #endif // AOCLIBS_PRINTFC_H_
