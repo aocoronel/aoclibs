200310b27083eaa3f19919054e02f3ed256c5915
Augusto Coronel <aocoronel@proton.me>
2026-06-04 17:05:46 -0300

    feat(calculus): add coerce_to_pow2

---
+++ include/calculus.h
@@ -0,0 +1,12 @@
+#ifndef AOCLIBS_CALCULUS_H_
+#define AOCLIBS_CALCULUS_H_
+
+#include "base.h"
+
+AOCDEF size_t coerce_to_pow2(size_t x);
+
+#ifdef AOCLIBS_IMPLEMENTATION
+#include "calculus.c"
+#endif
+
+#endif // AOCLIBS_CALCULUS_H_
---
+++ src/calculus.c
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "calculus.h"
+
+size_t coerce_to_pow2(size_t x) {
+    if (x < 2) return 2;
+    --x;
+    for (size_t s = 1; s < sizeof(size_t) * 8; s <<= 1)
+        x |= x >> s;
+    return x + 1;
+}
