187e046d063e3b7f94dc5ce9a9fa51248a8d2af7
Augusto Coronel <aocoronel@proton.me>
2026-06-17 19:38:38 -0300

    chore(libc): add comment

--- include/libc.h
+++ include/libc.h
@@ -1,6 +1,9 @@
 #ifndef AOCLIBS_LIBC_H_
 #define AOCLIBS_LIBC_H_
 
+// These are the most used LibC libraries, and all libraries from aoclibs are assumed to never
+// include these separatedly. You can take this into advantage to refactor them away.
+
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
