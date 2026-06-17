b96946cd50943a1771824d45578843e4be08d8e5
Augusto Coronel <aocoronel@proton.me>
2026-06-17 19:32:55 -0300

    refactor(base): add more comments, remove attributes without value, give macro parameters more explicit names. The UNREACHABLE macro now requires a string to tell why the code is unreachable. Add UNIMPLEMENTED macro. Add EXPECT macro. Rename macro WARN_UNUSED to MUST_USE. Renamed macro ATTR_NORETURN to NORETURN. Renamed macro ATTR_DEPRECATED to DEPRECATED. Removed ATTR_ALLOCS, ATTR_CONST, ATTR_PURE, ATTR_HOT, ATTR_COLD

--- include/base.h
+++ include/base.h
@@ -1,76 +1,99 @@
 #ifndef AOCLIBS_BASE_H_
 #define AOCLIBS_BASE_H_
 
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 700
+#endif
 
-// This is the foundation for all libraries
-
+// Most used LibC libraries
 #include "libc.h"
 
-// Modify prefixes in all functions:
-// #define AOCDEF static inline
+// AOCDEF can modify prefixes in all functions. Example:
+//  #define AOCDEF static inline
+//  #define AOCDEF extern "C"
+//
+// AOCLIBS won't use the "static" keyword to make functions "private", instead we just take the
+// convention of starting these functions with "_". The idea is that the provided functions can be
+// further reused.
 #ifndef AOCDEF
 #define AOCDEF
 #endif
 
-// === Pointers ===
-
-// Assumes all pointers at not nullable. Most, but not all, will be asserted.
-
-// "null" tells the pointer can be NULL
+// Assumes all pointers are not nullable. "null" tells the pointer can be NULL.
+// NOTE: If an argument accepts NULL or returns NULL, and the annotation is not present, assume it's
+// a design bug.
 #define null
 
+// All allocations are assumed to be made by malloc() and realloc(), and freed by free()
+// Because you can compile the entire library to just a single header file, you can overwrite it
+//  #define malloc(size) custom_malloc((size))
+
+// Compiler specific
+#define LIKELY(expr) __builtin_expect(!!(expr), 1)
+#define UNLIKELY(expr) __builtin_expect(!!(expr), 0)
+#define NORETURN __attribute__((noreturn))
+#define DEPRECATED(fn_to_use_instead) __attribute_deprecated_msg__(fn_to_use_instead)
+#define MUST_USE __attribute__((warn_unused_result))
+
+// The following macros are used to clarify intentions
+#define DISCARD(variable) (void)variable
+
+// Macros for renaming purposes
 #define restrict __restrict
+#define typeof(type) __typeof__((type))
+// C++ auto in C as a GNU extension: #define auto __auto_type
 
-// === Allocations ===
+// Convenient macros
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
-// All allocations are assumed to be made by malloc(), realloc() and freed by free()
-// Because you can compile the entire library to just a single header file, you can use
-// the "heap_trace.h" trick to change it.
+// CAT(ident, fier) -> identifier
+#define CAT(x, y) CAT_IMPL(x, y)
+#define CAT_IMPL(x, y) x##y
 
-// === Attributes ===
+// STRINGIFY(identifier) -> "identifier"
+#define STRINGIFY(x) #x
+// #define identifier 0
+// MSTRINGIFY(identifier) -> "0"
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-#define LIKELY(x) __builtin_expect(!!(x), 1)
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+// Using a third variable for swapping, turns this more generic than Xor
+#define SWAP(x, y)         \
+	do {                   \
+		typeof(x) tmp = x; \
+		x = y;             \
+		y = tmp;           \
+	} while (0)
 
-#define ATTR_ALLOCS(x) __attribute__((malloc, alloc_size(x)))
+// This is only applicable to stack allocated
+#define ARRAY_LEN(array) sizeof((array)) / sizeof((array[0]))
 
-#define ATTR_COLD __attribute__((cold))
-#define ATTR_HOT __attribute__((hot))
+// Compile-time strlen(). This expects a string literal
+#define STRLEN(string) ARRAY_LEN(("" string "")) - sizeof((string)[0])
 
-#define ATTR_NORETURN __attribute__((noreturn))
+// Unless is used for handling errors, so the control flow is always unlikely to happen
+#define unless(expr) if (UNLIKELY(expr))
 
-#define ATTR_PACKED __attribute__((__packed__))
+// range(0, 10, i) { printf("%d\n", i) }
+#define range(init, end, it) for (size_t it = (init); it < (end); it++)
 
-#define ATTR_CONST __attribute__((const))
-#define ATTR_PURE __attribute__((pure))
-
-#define ATTR_DEPRECATED(fn_to_use_instead) __attribute_deprecated_msg__(fn_to_use_instead)
-#define ATTR_WARN_UNUSED __attribute__((warn_unused_result))
-
-// To be used in function declarations. "static" is a very broad keyword in C,
-// internal express this idea better.
-#define internal __attribute__((visibility("hidden")));
-
-#define DISCARD(x) (void)x
-
-// === Debugging ===
-
+// aoclibs' abort()
 #define AOCLIBS_ABORT(msg, ...)                                    \
 	(fprintf(stderr, "%s: %s:%u: ", __func__, __FILE__, __LINE__), \
 	 fprintf(stderr, msg " " __VA_ARGS__),                         \
 	 fputc('\n', stderr),                                          \
 	 abort())
 
-// Used to panic, when an unreachable code is ran
-#define UNREACHABLE AOCLIBS_ABORT("Panicked: ", "unreachable code")
-
+// Panic when a code is assumed to never run
+#define UNREACHABLE(...) AOCLIBS_ABORT("Unreachable code: ", __VA_ARGS__)
 #define PANIC(...) AOCLIBS_ABORT("Panicked: ", __VA_ARGS__)
 
 // Marks not implemented code
 #define TODO(...) AOCLIBS_ABORT("TODO: ", __VA_ARGS__)
 
-// Asserts an expression, and prints a formatted message
+// int foo() { UNIMPLEMENTED; }
+#define UNIMPLEMENTED AOCLIBS_ABORT("Unimplemented function: ", __func__)
+
+// Asserts an expression, and prints an optional formatted message
 #ifdef NDEBUG
 #define ASSERT(...)
 #else
@@ -78,76 +101,49 @@
 	((expr) ? (void)0 : AOCLIBS_ABORT("Assertion failed: " #expr, __VA_ARGS__))
 #endif
 
+// Differently from ASSERT(), this one is not removed by NDEBUG
+#define EXPECT(expr, ...) \
+	((expr) ? (void)0 : AOCLIBS_ABORT("Assertion failed: " #expr, __VA_ARGS__))
+
 // Convenience assert messages
-#define ASSERT_NONNULL(exp) ASSERT((exp), "passing NULL pointer to Nonnull parameter")
+#define ASSERT_NONNULL(expr) ASSERT((expr), "passing NULL pointer to Nonnull parameter")
 
-// Convenient macros
+// === Sanitizers ===
 
-#define CAT(a, b) CAT_IMPL(a, b)
-#define CAT_IMPL(a, b) a##b
-
-#define STRINGIFY(x) #x
-
-// #define hello 0
-// MSTRINGIFY(hello) -> "0"
-#define MSTRINGIFY(x) STRINGIFY(x)
-
-#define SWAP(x, z)           \
-	do {                     \
-		__typeof__(x) t = x; \
-		x = z;               \
-		z = t;               \
-	} while (0)
-
-// This is only applicable to stack allocated
-#define ARRAY_LEN(a) sizeof((a)) / sizeof((a[0]))
-// Expects a string literal
-#define STRLEN(s) ARRAY_LEN(("" s "")) - sizeof((s)[0])
-
-#define eprintf(...) fprintf(stderr, __VA_ARGS__)
-
-// void foo() {
-//     void *p = malloc(1 << 20);
-//     unless (!p) abort();
-// }
-#define unless(expr) if (UNLIKELY(expr))
-
-#define range(init, end, it) for (size_t it = (init); it < (end); it++)
-
-// Sanitizers
+// These only take effect with -fsanitize=address
 
 #if defined(__SANITIZE_ADDRESS__)
 #include <sanitizer/asan_interface.h>
-#define _ASAN_POISON_MEMORY_REGION(region, size) ASAN_POISON_MEMORY_REGION((region), (size));
-#define _ASAN_UNPOISON_MEMORY_REGION(region, size) ASAN_UNPOISON_MEMORY_REGION((region), (size));
+#define _ASAN_POISON_MEMORY_REGION(ptr, size) ASAN_POISON_MEMORY_REGION((ptr), (size));
+#define _ASAN_UNPOISON_MEMORY_REGION(ptr, size) ASAN_UNPOISON_MEMORY_REGION((ptr), (size));
 #else
-#define _ASAN_POISON_MEMORY_REGION(region, size)
-#define _ASAN_UNPOISON_MEMORY_REGION(region, size)
+#define _ASAN_POISON_MEMORY_REGION(ptr, size)
+#define _ASAN_UNPOISON_MEMORY_REGION(ptr, size)
 #endif
 
 #if defined(__SANITIZE_MEMORY__)
 #include <sanitizer/msan_interface.h>
-#define __MSAN_POISON(region, size) __msan_poison((region), (size))
-#define __MSAN_UNPOISON(region, size) __msan_unpoison((region), (size))
+#define __MSAN_POISON(ptr, size) __msan_poison((ptr), (size))
+#define __MSAN_UNPOISON(ptr, size) __msan_unpoison((ptr), (size))
 #else
-#define __MSAN_POISON(region, size)
-#define __MSAN_UNPOISON(region, size)
+#define __MSAN_POISON(ptr, size)
+#define __MSAN_UNPOISON(ptr, size)
 #endif
 
 #if defined(__SANITIZE_MEMORY__) || defined(__SANITIZE_ADDRESS__)
 #define HAVE_SANITIZER
 #endif
 
-#define sanitizer_poison_memory(region, size)     \
-	do {                                          \
-		_ASAN_POISON_MEMORY_REGION(region, size); \
-		__MSAN_POISON(region, size);              \
+#define sanitizer_poison_memory(ptr, size)     \
+	do {                                       \
+		_ASAN_POISON_MEMORY_REGION(ptr, size); \
+		__MSAN_POISON(ptr, size);              \
 	} while (0)
 
-#define sanitizer_unpoison_memory(region, size)     \
-	do {                                            \
-		_ASAN_UNPOISON_MEMORY_REGION(region, size); \
-		__MSAN_UNPOISON(region, size);              \
+#define sanitizer_unpoison_memory(ptr, size)     \
+	do {                                         \
+		_ASAN_UNPOISON_MEMORY_REGION(ptr, size); \
+		__MSAN_UNPOISON(ptr, size);              \
 	} while (0)
 
 #endif // AOCLIBS_BASE_H_
