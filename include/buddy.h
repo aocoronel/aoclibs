33036b9f6a1d5f2295daf3a59ba0258b488935c2
Augusto Coronel <aocoronel@proton.me>
2026-07-19 13:49:41 -0300

    review(buddy): apply latest convention, and no private convention chore: buddy_alloc asserts on size == 0, since inspiration from malloc(0) is not portable

--- include/buddy.h
+++ include/buddy.h
@@ -1,5 +1,5 @@
-#ifndef AOCBLIS_BUDDY_H_H
-#define AOCBLIS_BUDDY_H_H
+#ifndef AOC_BUDDY_H
+#define AOC_BUDDY_H
 
 #include "base.h"
 
@@ -9,69 +9,68 @@
 // is out of space, we just create a new page.
 
 enum {
-	BUDDY_MIN_ORDER = 3, // 2^3 = 8 bytes
-	BUDDY_DEFAULT_PAGE_ORDER = 15, // 2^15 = 32768 bytes
-	BUDDY_MAX_ORDERS = 32,
+	Buddy_Min_Order = 3, // 2^3 = 8 bytes
+	Buddy_Default_Page_Order = 15, // 2^15 = 32768 bytes
+	Buddy_Max_Orders = 32,
 };
 
-typedef struct buddy_block {
-	struct buddy_block *next;
-} buddy_block_t;
+typedef struct Buddy_Block {
+	struct Buddy_Block *next;
+} Buddy_Block;
 
-typedef struct buddy_page {
-	struct buddy_page *next;
+typedef struct Buddy_Page {
+	struct Buddy_Page *next;
 	uint8_t *base;
 	uint8_t *split_map;
 	uint8_t *free_map;
-	buddy_block_t *free_lists[BUDDY_MAX_ORDERS];
+	Buddy_Block *free_lists[Buddy_Max_Orders];
 	unsigned max_order;
 	size_t total_size;
-} buddy_page_t;
+} Buddy_Page;
 
 typedef struct {
-	buddy_page_t *page;
+	Buddy_Page *page;
 	unsigned order;
-} buddy_header_t;
+} Buddy_Header;
 
 typedef struct {
-	buddy_page_t *begin;
-	buddy_page_t *end;
+	Buddy_Page *begin;
+	Buddy_Page *end;
 } Buddy;
 
-buddy_block_t *_buddy_freelist_pop(buddy_block_t **head);
-buddy_page_t *_buddy_new_page(unsigned page_order);
-int _buddy_freelist_remove(buddy_block_t **restrict head, buddy_block_t *restrict target);
-int _buddy_get_bit(uint8_t *map, size_t idx);
-size_t _buddy_block_count(unsigned max_order, unsigned order);
-size_t _buddy_node_index(unsigned max_order, unsigned order, size_t block_idx);
-size_t _buddy_order_size(unsigned order);
-void *_buddy_alloc_page(buddy_page_t *p, size_t bytes);
-void _buddy_free(buddy_page_t *p);
-void _buddy_free_page(buddy_page_t *restrict p, void *restrict ptr, unsigned order);
-void _buddy_freelist_push(buddy_block_t **restrict head, buddy_block_t *restrict blk);
-void _buddy_set_bit(uint8_t *map, size_t idx, int value);
+AOCDEF Buddy_Block *buddy_freelist_pop(Buddy_Block **head);
+AOCDEF Buddy_Page *buddy_new_page(unsigned page_order);
+AOCDEF int buddy_freelist_remove(Buddy_Block **restrict head, Buddy_Block *restrict target);
+AOCDEF int buddy_get_bit(uint8_t *map, size_t idx);
+AOCDEF size_t buddy_block_count(unsigned max_order, unsigned order);
+AOCDEF size_t buddy_node_index(unsigned max_order, unsigned order, size_t block_idx);
+AOCDEF size_t buddy_order_size(unsigned order);
+AOCDEF void *buddy_alloc_page(Buddy_Page *p, size_t bytes);
+AOCDEF void buddy_free_page(Buddy_Page *restrict p, void *restrict ptr, unsigned order);
+AOCDEF void buddy_freelist_push(Buddy_Block **restrict head, Buddy_Block *restrict blk);
+AOCDEF void buddy_set_bit(uint8_t *map, size_t idx, int value);
 
 // Public API
-void *buddy_alloc(Buddy *a, size_t size_bytes);
-void buddy_dealloc(void *ptr);
-void buddy_free(Buddy *a);
+AOCDEF void *buddy_alloc(Buddy *b, size_t size_bytes);
+AOCDEF void buddy_dealloc(void *ptr);
+AOCDEF void buddy_free(Buddy *b);
 
+#ifdef TUNIT
+TEST(buddy_alloc) {
+	Buddy a = { 0 };
+
+	void *p4 = buddy_alloc(&a, 513);
+	void *p1 = buddy_alloc(&a, 64);
+	void *p2 = buddy_alloc(&a, 128);
+	void *p3 = buddy_alloc(&a, 300);
+
+	buddy_dealloc(p2);
+	buddy_dealloc(p1);
+	buddy_dealloc(p3);
+	buddy_dealloc(p4);
+
+	buddy_free(&a);
+}
 #endif
 
-// int main(void) {
-//     Buddy a = { 0 };
-//
-//     void *p4 = buddy_alloc(&a, 513);
-//     void *p1 = buddy_alloc(&a, 64);
-//     void *p2 = buddy_alloc(&a, 128);
-//     void *p3 = buddy_alloc(&a, 300);
-//
-//     buddy_dealloc(p2);
-//     buddy_dealloc(p1);
-//     buddy_dealloc(p3);
-//     buddy_dealloc(p4);
-//
-//     buddy_free(&a);
-//
-//     return 0;
-// }
+#endif // AOC_BUDDY_H
--- src/buddy.c
+++ src/buddy.c
@@ -2,24 +2,24 @@
 
 #include "buddy.h"
 
-size_t _buddy_order_size(unsigned order) {
+size_t buddy_order_size(unsigned order) {
 	return ((size_t)1) << order;
 }
 
-size_t _buddy_block_count(unsigned max_order, unsigned order) {
+size_t buddy_block_count(unsigned max_order, unsigned order) {
 	return ((size_t)1) << (max_order - order);
 }
 
-size_t _buddy_node_index(unsigned max_order, unsigned order, size_t block_idx) {
+size_t buddy_node_index(unsigned max_order, unsigned order, size_t block_idx) {
 	return (((size_t)1) << (max_order - order)) - 1 + block_idx;
 }
 
-int _buddy_get_bit(uint8_t *map, size_t idx) {
+int buddy_get_bit(uint8_t *map, size_t idx) {
 	$assert_nonnull(map);
 	return (map[idx >> 3] >> (idx & 7)) & 1;
 }
 
-void _buddy_set_bit(uint8_t *map, size_t idx, int value) {
+void buddy_set_bit(uint8_t *map, size_t idx, int value) {
 	$assert_nonnull(map);
 	if (value)
 		map[idx >> 3] |= (1u << (idx & 7));
@@ -27,22 +27,29 @@ void _buddy_set_bit(uint8_t *map, size_t idx, int value) {
 		map[idx >> 3] &= ~(1u << (idx & 7));
 }
 
-buddy_page_t *_buddy_new_page(unsigned page_order) {
-	size_t tree_nodes = (((size_t)1) << (page_order - BUDDY_MIN_ORDER + 1)) - 1;
+Buddy_Page *buddy_new_page(unsigned page_order) {
+	size_t tree_nodes = (((size_t)1) << (page_order - Buddy_Min_Order + 1)) - 1;
 
 	size_t map_bytes = (tree_nodes + 7) / 8;
-	size_t total_size = _buddy_order_size(page_order);
+	size_t total_size = buddy_order_size(page_order);
 
-	size_t allocated_size = sizeof(buddy_page_t) + /* struct */
+	size_t allocated_size = sizeof(Buddy_Page) + /* struct */
 							map_bytes + /* split_map */
 							map_bytes + /* free_map */
 							total_size; /* base */
 
+	// This is a classical place where you would have to allocate for 4 different things, check
+	// if they are not NULL, free everything previous to it and then return failure.
+	// We prevent this poor habit, that modern developers solve with `defer`. The true best solution
+	// is to make one syscall and allocate everything into a single buffer, which is equally
+	// only one syscall to free.
+	// No malloc/free chase, the only place this API ever allocates is here.
+	// TODO: when sanitizer is enabled, poison the base memory when returning
 	uint8_t *buff = (uint8_t *)calloc(1, allocated_size);
 	$catch(!buff) return NULL;
 
-	buddy_page_t *p = (buddy_page_t *)buff;
-	buff += sizeof(buddy_page_t);
+	Buddy_Page *p = (Buddy_Page *)buff;
+	buff += sizeof(Buddy_Page);
 
 	p->max_order = page_order;
 	p->total_size = total_size;
@@ -56,46 +63,41 @@ buddy_page_t *_buddy_new_page(unsigned page_order) {
 
 	p->base = buff;
 
-	for (unsigned i = 0; i < BUDDY_MAX_ORDERS; ++i)
+	for (unsigned i = 0; i < Buddy_Max_Orders; ++i)
 		p->free_lists[i] = NULL;
 
-	buddy_block_t *root = (buddy_block_t *)p->base;
+	Buddy_Block *root = (Buddy_Block *)p->base;
 	root->next = NULL;
 
 	p->free_lists[page_order] = root;
 
-	size_t root_idx = _buddy_node_index(page_order, page_order, 0);
+	size_t root_idx = buddy_node_index(page_order, page_order, 0);
 
-	_buddy_set_bit(p->free_map, root_idx, 1);
-	_buddy_set_bit(p->split_map, root_idx, 0);
+	buddy_set_bit(p->free_map, root_idx, 1);
+	buddy_set_bit(p->split_map, root_idx, 0);
 
 	return p;
 }
 
-void _buddy_free(buddy_page_t *p) {
-	$assert_nonnull(p);
-	free(p);
-}
-
-void _buddy_freelist_push(buddy_block_t **head, buddy_block_t *blk) {
+void buddy_freelist_push(Buddy_Block **head, Buddy_Block *blk) {
 	$assert_nonnull(head);
 	$assert_nonnull(blk);
 	blk->next = *head;
 	*head = blk;
 }
 
-buddy_block_t *_buddy_freelist_pop(buddy_block_t **head) {
+Buddy_Block *buddy_freelist_pop(Buddy_Block **head) {
 	$assert_nonnull(head);
-	buddy_block_t *r = *head;
+	Buddy_Block *r = *head;
 	if (r) *head = r->next;
 	return r;
 }
 
-int _buddy_freelist_remove(buddy_block_t **head, buddy_block_t *target) {
+int buddy_freelist_remove(Buddy_Block **head, Buddy_Block *target) {
 	$assert_nonnull(head);
 	$assert_nonnull(target);
-	buddy_block_t *prev = NULL;
-	buddy_block_t *cur = *head;
+	Buddy_Block *prev = NULL;
+	Buddy_Block *cur = *head;
 
 	while (cur) {
 		if (cur == target) {
@@ -112,12 +114,13 @@ int _buddy_freelist_remove(buddy_block_t **head, buddy_block_t *target) {
 	return 0;
 }
 
-void *_buddy_alloc_page(buddy_page_t *p, size_t bytes) {
+// TODO: unpoison requested memory
+void *buddy_alloc_page(Buddy_Page *p, size_t bytes) {
 	$assert_nonnull(p);
-	size_t needed = bytes + sizeof(buddy_header_t);
+	size_t needed = bytes + sizeof(Buddy_Header);
 
-	unsigned order = BUDDY_MIN_ORDER;
-	while (order < p->max_order && _buddy_order_size(order) < needed)
+	unsigned order = Buddy_Min_Order;
+	while (order < p->max_order && buddy_order_size(order) < needed)
 		order++;
 
 	$catch(order > p->max_order) return NULL;
@@ -129,73 +132,71 @@ void *_buddy_alloc_page(buddy_page_t *p, size_t bytes) {
 
 	$catch(current > p->max_order) return NULL;
 
-	buddy_block_t *blk = _buddy_freelist_pop(&p->free_lists[current]);
+	Buddy_Block *blk = buddy_freelist_pop(&p->free_lists[current]);
 
 	size_t blk_idx = ((uintptr_t)((uint8_t *)blk - p->base)) >> current;
 
-	_buddy_set_bit(p->free_map, _buddy_node_index(p->max_order, current, blk_idx), 0);
+	buddy_set_bit(p->free_map, buddy_node_index(p->max_order, current, blk_idx), 0);
 
 	while (current > order) {
 		current--;
 
-		size_t half = _buddy_order_size(current);
+		size_t half = buddy_order_size(current);
 
-		buddy_block_t *right = (buddy_block_t *)((uint8_t *)blk + half);
+		Buddy_Block *right = (Buddy_Block *)((uint8_t *)blk + half);
 
-		_buddy_freelist_push(&p->free_lists[current], right);
+		buddy_freelist_push(&p->free_lists[current], right);
 
 		size_t left_idx = blk_idx << 1;
 		size_t right_idx = left_idx + 1;
 
-		_buddy_set_bit(p->split_map, _buddy_node_index(p->max_order, current + 1, blk_idx), 1);
+		buddy_set_bit(p->split_map, buddy_node_index(p->max_order, current + 1, blk_idx), 1);
 
-		_buddy_set_bit(p->free_map, _buddy_node_index(p->max_order, current, right_idx), 1);
+		buddy_set_bit(p->free_map, buddy_node_index(p->max_order, current, right_idx), 1);
 
 		blk_idx = left_idx;
 	}
 
-	buddy_header_t *hdr = (buddy_header_t *)blk;
+	Buddy_Header *hdr = (Buddy_Header *)blk;
 	hdr->page = p;
 	hdr->order = order;
 
 	return (void *)(hdr + 1);
 }
 
-void *buddy_alloc(Buddy *a, size_t size_bytes) {
-	$assert_nonnull(a);
+void *buddy_alloc(Buddy *b, size_t size_bytes) {
+	$assert_nonnull(b);
+	$assert(size_bytes > 0, "invalid allocation size");
 
-	// Should we assert this instead?
-	$catch(size_bytes == 0) return NULL;
-
-	buddy_page_t *p = a->begin;
+	Buddy_Page *p = b->begin;
 
 	while (p) {
-		void *r = _buddy_alloc_page(p, size_bytes);
+		void *r = buddy_alloc_page(p, size_bytes);
 		if (r) return r;
 		p = p->next;
 	}
 
-	unsigned needed_order = BUDDY_MIN_ORDER;
-	while (_buddy_order_size(needed_order) < size_bytes + sizeof(buddy_header_t))
+	unsigned needed_order = Buddy_Min_Order;
+	while (buddy_order_size(needed_order) < size_bytes + sizeof(Buddy_Header))
 		needed_order++;
 
-	unsigned page_order = BUDDY_DEFAULT_PAGE_ORDER;
+	unsigned page_order = Buddy_Default_Page_Order;
 	if (needed_order > page_order) page_order = needed_order;
 
-	buddy_page_t *newp = _buddy_new_page(page_order);
+	Buddy_Page *newp = buddy_new_page(page_order);
 	$catch(!newp) return NULL;
 
-	if (a->end)
-		a->end->next = newp;
+	if (b->end)
+		b->end->next = newp;
 	else
-		a->begin = newp;
+		b->begin = newp;
 
-	a->end = newp;
+	b->end = newp;
 
-	return _buddy_alloc_page(newp, size_bytes);
+	return buddy_alloc_page(newp, size_bytes);
 }
 
-void _buddy_free_page(buddy_page_t *p, void *ptr, unsigned order) {
+void buddy_free_page(Buddy_Page *p, void *ptr, unsigned order) {
 	$assert_nonnull(p);
 	$assert_nonnull(ptr);
 	uint8_t *blk = (uint8_t *)ptr;
@@ -206,19 +207,19 @@ void _buddy_free_page(buddy_page_t *p, void *ptr, unsigned order) {
 		size_t buddy_idx = blk_idx ^ 1;
 
 		size_t buddy_offset = buddy_idx << order;
-		buddy_block_t *buddy = (buddy_block_t *)(p->base + buddy_offset);
+		Buddy_Block *buddy = (Buddy_Block *)(p->base + buddy_offset);
 
-		size_t buddy_node = _buddy_node_index(p->max_order, order, buddy_idx);
+		size_t buddy_node = buddy_node_index(p->max_order, order, buddy_idx);
 
-		if (!_buddy_get_bit(p->free_map, buddy_node)) break;
+		if (!buddy_get_bit(p->free_map, buddy_node)) break;
 
-		if (!_buddy_freelist_remove(&p->free_lists[order], buddy)) break;
+		if (!buddy_freelist_remove(&p->free_lists[order], buddy)) break;
 
-		_buddy_set_bit(p->free_map, buddy_node, 0);
+		buddy_set_bit(p->free_map, buddy_node, 0);
 
 		size_t parent_idx = blk_idx >> 1;
 
-		_buddy_set_bit(p->split_map, _buddy_node_index(p->max_order, order + 1, parent_idx), 0);
+		buddy_set_bit(p->split_map, buddy_node_index(p->max_order, order + 1, parent_idx), 0);
 
 		blk_idx = parent_idx;
 		blk = p->base + (blk_idx << (order + 1));
@@ -226,32 +227,33 @@ void _buddy_free_page(buddy_page_t *p, void *ptr, unsigned order) {
 		order++;
 	}
 
-	buddy_block_t *final_blk = (buddy_block_t *)blk;
+	Buddy_Block *final_blk = (Buddy_Block *)blk;
 
-	_buddy_freelist_push(&p->free_lists[order], final_blk);
+	buddy_freelist_push(&p->free_lists[order], final_blk);
 
-	_buddy_set_bit(p->free_map, _buddy_node_index(p->max_order, order, blk_idx), 1);
+	buddy_set_bit(p->free_map, buddy_node_index(p->max_order, order, blk_idx), 1);
 }
 
 void buddy_dealloc(void *ptr) {
 	$assert_nonnull(ptr);
 
-	buddy_header_t *hdr = ((buddy_header_t *)ptr) - 1;
+	Buddy_Header *hdr = ((Buddy_Header *)ptr) - 1;
 
-	_buddy_free_page(hdr->page, hdr, hdr->order);
+	buddy_free_page(hdr->page, hdr, hdr->order);
 }
 
-void buddy_free(Buddy *a) {
-	$assert_nonnull(a);
+void buddy_free(Buddy *b) {
+	$assert_nonnull(b);
 
-	buddy_page_t *p = a->begin;
+	Buddy_Page *p = b->begin;
 
 	while (p) {
-		buddy_page_t *next = p->next;
-		_buddy_free(p);
+		Buddy_Page *next = p->next;
+		$assert(next, "double free attempt");
+		free(p);
 		p = next;
 	}
 
-	a->begin = NULL;
-	a->end = NULL;
+	b->begin = NULL;
+	b->end = NULL;
 }
