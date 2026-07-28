89f7fb2fe2c44811eccad2ccd6dc197c03892dc3
Augusto Coronel <aocoronel@proton.me>
2026-07-28 14:29:57 -0300

    docs(tunit): turn table into bullet points for better readability

--- include/tunit.h
+++ include/tunit.h
@@ -1,24 +1,31 @@
 #ifndef AOC_TUNIT_H_
 #define AOC_TUNIT_H_
 
-#if 0
+/*
 TUnit -- Test Unit
 
 TUnit is probably the most powerful and lightweight testing unit library you will ever see. Although the usage looks very similar to [Criterion](https://github.com/Snaipe/Criterion). In fact, TUnit is a single header library. TUnit is not just simple, but also completely independent from `aoclibs`, so you can just download `tunit.h` and play around with it.
 
-|                               | Criterion                                                                                       | TUnit                                                                         |
-|-------------------------------|-------------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------|
-| LOC                           | C: 7559; Header: 10815 (0586ad3 - bleeding)                                                     | 313 (4524079 - main)                                                          |
-| Dependencies                  | openssl, libffi, libgit2, libnanomsg, libllhttp, libpcre2, libssh2                              | None                                                                          |
-| Build                         | 500 lines of Meson to make, then you need to link                                               | `cc -DTUNIT`                                                                  |
-| Speed on single test `0 == 0` | 0.40ms                                                                                          | 0.02ms single/multiprocess (-O0)                                              |
-| Ease of use                   | They have a documentation                                                                       | The only example you need is right below, you just need to know C to use this |
-| Unique Features               | xUnit framework structure, parameterized tests and theories Subprocess tests only, report hooks | Define tests alongside the source code, and optionally detect leaks in tests  |
-| Platform support              | Linux, FreeBSD, macOS, and Windows                                                              | Linux and FreeBSD                                                             |
+- Criterion:
+  - LOC                          : C: 7559; Header: 10815 (0586ad3 - bleeding)
+  - Dependencies                 : openssl, libffi, libgit2, libnanomsg, libllhttp, libpcre2, libssh2
+  - Build                        : 500 lines of Meson to make, then you need to link
+  - Speed on single test `0 == 0`: 0.40ms
+  - Ease of use                  : They have a documentation
+  - Unique Features              : Parameterized tests and theories, and report hooks
+  - Platform support             : Linux, FreeBSD, macOS, and Windows
+- TUnit:
+  - LOC                          : 316 (2a3ef77 - main)
+  - Dependencies                 : None
+  - Build                        : `cc -DTUNIT`
+  - Speed on single test `0 == 0`: 0.020ms single/multiprocess (-O0)
+  - Ease of use                  : The only example you need is right below, you just need to know C to use this
+  - Unique Features              : Define tests alongside the source code
+  - Platform support             : Linux and FreeBSD
 
-`tunit.h` has an optional dependency on `heap_trace.h`, which finds `malloc`, `calloc`, `realloc` and `free` based leaks, and tells where in the source code that happened.
+tunit.h has an optional dependency on heap_trace.h, which finds malloc, calloc, realloc and free based leaks, and tells where in the source code that happened.
 
-TUnit is inspired by the Zig test unit feature, that lets you write tests alongside the actual source code, instead of in a separate `test/` directory with separate tests files, that needs to be compiled separatedly. TUnit lets you compile all tests at once and run everything in a single process, or in subprocesses (trading-off `heap_trace` in the process).
+TUnit is inspired by the Zig test unit feature, that lets you write tests alongside the actual source code, instead of in a separate test/ directory with separate tests files, that needs to be compiled separatedly. TUnit lets you compile all tests at once and run everything in a single process, or in subprocesses (trading-off heap_trace in the process).
 
 Currently, there is no support for running tests in several threads.
 
@@ -68,8 +75,8 @@ Output:
 0 succeed, 1 failed (0.01ms total)
 ```
 
-Notice we just compile the entirety of `tunit.h` without optimization flags, and the execution was fast.
-#endif
+Notice we just compile the entirety of tunit.h without optimization flags, and the execution was fast.
+*/
 
 // Enable TUnit :: cc -DTUNIT
 
